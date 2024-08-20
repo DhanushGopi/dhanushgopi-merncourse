f8a5862ab025f83dcb3dd472f16918ac23e4c53d
DhanushGopi <g.dhanushgopi1509@gmail.com>
2024-08-21 01:09:16 +0530

    updating the contact form in task1

--- weekend task/weekend1 tasks/task-1/script/script1.js
+++ weekend task/weekend1 tasks/task-1/script/script1.js
@@ -5,22 +5,42 @@ function showLogin() {
     document.querySelector('.loginbtn').classList.add("activebtn");
     document.querySelector('.registerbtn').classList.add("unactivebtn");
     document.querySelector('.registerbtn').classList.remove("activebtn");
+    document.querySelector('.contactbtn').classList.add("unactivebtn");
+    document.querySelector('.contactbtn').classList.remove("activebtn");
     document.querySelector(".loginform").classList.remove("formhide");
+    document.querySelector(".contactform").classList.add("formhide");
     document.querySelector(".registerform").classList.add("formhide");
 };
 
-document.querySelector("#loginbtn").addEventListener('click', showLogin);
+document.querySelector(".loginbtn").addEventListener('click', showLogin);
 
 function showRegister() {
     document.querySelector('.registerbtn').classList.remove("unactivebtn");
     document.querySelector('.registerbtn').classList.add("activebtn");
     document.querySelector('.loginbtn').classList.add("unactivebtn");
     document.querySelector('.loginbtn').classList.remove("activebtn");
+    document.querySelector('.contactbtn').classList.add("unactivebtn");
+    document.querySelector('.contactbtn').classList.remove("activebtn");
     document.querySelector(".registerform").classList.remove("formhide");
+    document.querySelector(".contactform").classList.add("formhide");
     document.querySelector(".loginform").classList.add("formhide");
 };
 
-document.querySelector("#registerbtn").addEventListener('click', showRegister);
+document.querySelector(".registerbtn").addEventListener('click', showRegister);
+
+function showContact() {
+    document.querySelector('.registerbtn').classList.add("unactivebtn");
+    document.querySelector('.registerbtn').classList.remove("activebtn");
+    document.querySelector('.loginbtn').classList.add("unactivebtn");
+    document.querySelector('.loginbtn').classList.remove("activebtn");
+    document.querySelector('.contactbtn').classList.remove("unactivebtn");
+    document.querySelector('.contactbtn').classList.add("activebtn");
+    document.querySelector(".registerform").classList.add("formhide");
+    document.querySelector(".contactform").classList.remove("formhide");
+    document.querySelector(".loginform").classList.add("formhide");
+};
+
+document.querySelector(".contactbtn").addEventListener('click', showContact);
 
 
 /* pages nav*/
